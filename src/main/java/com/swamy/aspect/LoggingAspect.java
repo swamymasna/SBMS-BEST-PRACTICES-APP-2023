9f730231143b9d3962bf97ff3bb27428eee62574
swamymasna <swamymasna55@gmail.com>
2023-09-03 08:33:19 +0530

    Added AOP Code

--- src/main/java/com/swamy/aspect/LoggingAspect.java
+++ src/main/java/com/swamy/aspect/LoggingAspect.java
@@ -3,6 +3,7 @@ package com.swamy.aspect;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -14,14 +15,19 @@ import lombok.extern.slf4j.Slf4j;
 @Component
 public class LoggingAspect {
 
-	@Around(value = "execution(* com.swamy.controller.*.*(..))")
-	public Object aroundAdviceForController(ProceedingJoinPoint joinPoint) throws Throwable {
+	@Pointcut(value = "execution(* com.swamy.service.*.*(..)) || execution(* com.swamy.controller.*.*(..))")
+	public void myPointcut() {
+
+	}
+
+	@Around("myPointcut()")
+	public Object aroundAdvice(ProceedingJoinPoint joinPoint) throws Throwable {
 
 		ObjectMapper mapper = new ObjectMapper();
 
 		String methodName = joinPoint.getSignature().getName();
 
-		String className = joinPoint.getTarget().getClass().toString();
+		String className = joinPoint.getTarget().getClass().getName();
 
 		Object[] args = joinPoint.getArgs();
 
@@ -36,27 +42,5 @@ public class LoggingAspect {
 
 		return object;
 	}
-	
-	@Around(value = "execution(* com.swamy.service.*.*(..))")
-	public Object aroundAdviceForService(ProceedingJoinPoint joinPoint) throws Throwable {
 
-		ObjectMapper mapper = new ObjectMapper();
-
-		String methodName = joinPoint.getSignature().getName();
-
-		String className = joinPoint.getTarget().getClass().toString();
-
-		Object[] args = joinPoint.getArgs();
-
-		log.info("Method Invoked : " + className + " : " + methodName + "() " + "Arguments : "
-				+ mapper.writeValueAsString(args));
-
-		Object object = joinPoint.proceed();
-
-		log.info(className + " : " + methodName + "() " + "Response : " + mapper.writeValueAsString(object));
-
-		log.info("Returning Back from : " + className + " : " + methodName + "() method");
-		
-		return object;
-	}
 }
